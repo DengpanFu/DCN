265c7c785954c6cc4c524b25cd818cb7b469c32b
DengpanFu <fdpan@mail.ustc.edu.cn>
2019-12-24 15:51:13 +0800

    philly

--- dcn3d/setup.py
+++ dcn3d/setup.py
@@ -11,7 +11,6 @@ from torch.utils.cpp_extension import CUDAExtension
 from setuptools import find_packages
 from setuptools import setup
 
-requirements = ["torch", "torchvision"]
 
 def get_extensions():
     this_dir = os.path.dirname(os.path.abspath(__file__))
@@ -22,22 +21,18 @@ def get_extensions():
     source_cuda = glob.glob(os.path.join(extensions_dir, "cuda", "*.cu"))
 
     sources = main_file + source_cpu
-    extension = CppExtension
     extra_compile_args = {"cxx": []}
     define_macros = []
 
-    if torch.cuda.is_available():
-        extension = CUDAExtension
-        sources += source_cuda
-        define_macros += [("WITH_CUDA", None)]
-        extra_compile_args["nvcc"] = [
-            "-DCUDA_HAS_FP16=1",
-            "-D__CUDA_NO_HALF_OPERATORS__",
-            "-D__CUDA_NO_HALF_CONVERSIONS__",
-            "-D__CUDA_NO_HALF2_OPERATORS__",
-        ]
-    else:
-        raise NotImplementedError('Cuda is not availabel')
+    extension = CUDAExtension
+    sources += source_cuda
+    define_macros += [("WITH_CUDA", None)]
+    extra_compile_args["nvcc"] = [
+        "-DCUDA_HAS_FP16=1",
+        "-D__CUDA_NO_HALF_OPERATORS__",
+        "-D__CUDA_NO_HALF_CONVERSIONS__",
+        "-D__CUDA_NO_HALF2_OPERATORS__",
+    ]
 
     sources = [os.path.join(extensions_dir, s) for s in sources]
     include_dirs = [extensions_dir]
@@ -59,7 +54,6 @@ setup(
     url="https://github.com/charlesshang/DCNv2",
     description="deformable convolutional networks",
     packages=find_packages(exclude=("configs", "tests",)),
-    # install_requires=requirements,
     ext_modules=get_extensions(),
     cmdclass={"build_ext": torch.utils.cpp_extension.BuildExtension},
 )
